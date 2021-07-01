aa29f8e3a5c73fd8dbb1f002638ea0ab3b1488e6
Sprtr <shane.porter@ucdconnect.ie>
2021-07-01 18:20:08 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#github and version control tutorial continued
+#2nf
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print('Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Kkkkk
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print('UCD PA')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
